64ea89827be68dbf471305f7d64eabd7973e56e0
Akash <akashpatel1098@gmail.com>
2021-09-20 17:05:19 +0530

    not started project

--- myFirstProject/src/App.java
+++ myFirstProject/src/App.java
@@ -1,5 +1,35 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+ class testClass {
+     static int a=5;
+     int b=9;
+    static void meth(){
+        System.out.println("meth");
+        System.out.println(a);
+        // System.out.println(b);
     }
+    void meth3(){
+        System.out.println("meth2");
+        System.out.println(a);
+        System.out.println(b);
+    }
+    
+}
+public class App {
+    int b=7;
+    void meth2(){
+        System.out.println(b);
+    }
+
+    
+    public static void main(String[] args) throws Exception {
+        
+        
+        System.out.println("Hello, World!");
+        testClass t1=new testClass();
+        t1.meth3();
+        // t1.meth();
+        testClass.meth();
+        // testClass.meth();
+       
+}
 }
