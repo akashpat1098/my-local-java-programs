d9dee17039c37ef0900500dd8b2589a59aeb0226
Akash <akashpatel1098@gmail.com>
2022-02-06 22:36:46 +0530

    Installed ssd so had to config path of java hence has to do some settings

--- myFirstProject/src/Employee.java
+++ myFirstProject/src/Employee.java
@@ -1,45 +1,43 @@
-class Employee2
-{
-private String name,address;
-private int year; 
-private double salary;
-public Employee2(String n,int y,double z,String x)
-{	
-name = n;
-address = x;
-year = y;
-salary = z;
+class Employee2 {
+    private String name, address;
+    private int year;
+    private double salary;
+
+    public Employee2(String n, int y, double z, String x) {
+        name = n;
+        address = x;
+        year = y;
+        salary = z;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public String getAddress() {
+        return address;
+    }
 }
-public String getName()
-{
-return name;
-}
-public int getYear()
-{
-return year;
-}
-public double getSalary()
-{
-return salary;
-}
-public String getAddress()
-{
-return address;
-}
-}
-class Employee
-{
-public static void main(String args[])
-{
-Employee2 e1 = new Employee2("Robert", 1994, 50000, "64C- WallsStreet");
-Employee2 e2 = new Employee2("Sam", 2000, 74000, "68D- WallsStreet");
-Employee2 e3 = new Employee2("John", 1999, 60000, "26B- WallsStreet");
-System.out.println("Name\tYearOfJoining\tSalary\t\tAddress");
-System.out.println(e1.getName()+"\t"+e1.getYear()+"\t\t"+e1.getSalary()+"\t\t"
-+e1.getAddress());
-System.out.println(e2.getName()+"\t"+e2.getYear()+"\t\t"+e2.getSalary()+"\t\t"
-+e2.getAddress());
-System.out.println(e3.getName()+"\t"+e3.getYear()+"\t\t"+e3.getSalary()+"\t\t"
-+e3.getAddress());
-}
+
+class Employee {
+    public static void main(String args[]) {
+        Employee2 e1 = new Employee2("Robert", 1994, 50000, "64C- WallsStreet");
+        Employee2 e2 = new Employee2("Sam", 2000, 74000, "68D- WallsStreet");
+        Employee2 e3 = new Employee2("John", 1999, 60000, "26B- WallsStreet");
+        System.out.println("Name\tYearOfJoining\tSalary\t\tAddress");
+        System.out.println(e1.getName() + "\t" + e1.getYear() + "\t\t" + e1.getSalary() + "\t\t"
+                + e1.getAddress());
+        System.out.println(e2.getName() + "\t" + e2.getYear() + "\t\t" + e2.getSalary() + "\t\t"
+                + e2.getAddress());
+        System.out.println(e3.getName() + "\t" + e3.getYear() + "\t\t" + e3.getSalary() + "\t\t"
+                + e3.getAddress());
+    }
 }
--- myFirstProject/src/practice.java
+++ myFirstProject/src/practice.java
@@ -1,5 +1,5 @@
 public class practice {
     public static void main(String[] args) {
-        System.out.println("Hello wolrd");
+        System.out.println("Hello wolrd1");
     }
 }
